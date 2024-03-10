3872f249224ff2f854efbd6a2056aff08ee7070e
suguinamae <rabin1381@fmail.com>
2024-03-10 15:21:12 -0800

    mypic error in positions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Mypic from "./components/mypic/mypic";
 
 function App() {
   return <div className="App">
+    <Mypic></Mypic>
   </div>;
 }
 

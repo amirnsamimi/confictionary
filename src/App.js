cead750bc3acd920e0f5066ba3c573707a080269
suguinamae <rabin1381@fmail.com>
2024-03-10 13:01:58 -0800

    slideshow done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Slideshow from "./components/slide show/slideshow";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Slideshow/>
+  </div>;
 }
 
 export default App;
---
+++ src/components/slide show/slideshow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./slideshow.style.scss";
+const Slideshow = () => {
+  return (
+    <div className="allpage">
+      <div className="slideshow">
+        <p className="txt">OUR ADVANTAGES</p>
+      </div>
+    </div>
+  );
+};
+
+export default Slideshow;

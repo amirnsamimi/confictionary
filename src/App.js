76e077fe19d24800918fff2480e2a0d4613ea458
suguinamae <rabin1381@fmail.com>
2024-03-10 14:45:05 -0800

    slide show done2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Slideshow from "./components/slide show/slideshow";
 
 function App() {
   return <div className="App">
-    <Slideshow/>
   </div>;
 }
 

b50739e01852239c0ed8a2d505e265ae82ce70ac
suguinamae <rabin1381@fmail.com>
2024-03-11 00:39:02 -0800

    my test done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 
-function App() {
-  return <div className="App"></div>;
-=======
 import Mylayout from "./components/Mylayout/Mylayout";
 function App() {
   return <div className="App">
     <Mylayout></Mylayout>
   </div>;
->>>>>>> mytest
 }
 
 export default App;
--- src/components/Mylayout/Mylayout.jsx
+++ src/components/Mylayout/Mylayout.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Myheader from "../myheader/Myheader"
-import Slideshow from "../slide show/slideshow"
-import Mypic from "../mypic/mypic"
+import React from "react";
+import Myheader from "../myheader/Myheader";
+import Slideshow from "../slide show/slideshow";
+import Mypic from "../mypic/mypic";
+import FooterOption from "../FooterOption/FooterOption";
+import "./Mylayout.style.scss";
 
 const Mylayout = () => {
   return (
-    <div className='fullpage'>
+    <div className="fullpage">
+      <div className="hder">
         <Myheader></Myheader>
+      </div>
+      <div className="abs">
         <Slideshow></Slideshow>
         <Mypic></Mypic>
-
+      </div>
+      <div className="fter">
+        <FooterOption></FooterOption>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mylayout
+export default Mylayout;

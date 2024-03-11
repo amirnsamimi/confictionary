b335c01f992fe2ee85ceb8fe13588eaadc25581e
suguinamae <rabin1381@fmail.com>
2024-03-10 22:41:11 -0800

    mypic done2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Mypic from "./components/mypic/mypic";
 
 function App() {
   return <div className="App">
-    <Mypic></Mypic>
   </div>;
 }
 
